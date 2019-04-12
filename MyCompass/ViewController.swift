8167c295a7712ac9efc448feafb165ff08c82db7
serge <pyrov.sergey@gmail.com>
2019-04-12 17:35:05 +0300

    Initial Commit

---
+++ MyCompass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCompass
+//
+//  Created by Sergey on 12/04/2019.
+//  Copyright © 2019 PyrovSergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
